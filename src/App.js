22a5d9e8669af51277bfe245e83c32da7b948f7b
Afsha Zareen <afshazareen117@gmail.com>
2025-03-23 15:36:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import StudentAssessment from "./StudentAssessment";
+import ClassPerformance from "./ClassPerformance";
+import { Button, Container, Box } from "@mui/material";
+
+function App() {
+  const [view, setView] = useState("class"); // Toggle between views
+
+  return (
+    <Container sx={{ textAlign: "center", marginTop: 4 }}>
+      <Box sx={{ marginBottom: 3 }}>
+        <Button variant={view === "individual" ? "contained" : "outlined"} onClick={() => setView("individual")} sx={{ marginRight: 2 }}>
+          Individual Student
+        </Button>
+        <Button variant={view === "class" ? "contained" : "outlined"} onClick={() => setView("class")}>
+          Class Performance
+        </Button>
+      </Box>
+
+      {view === "individual" ? <StudentAssessment /> : <ClassPerformance />}
+    </Container>
+  );
+}
+
+export default App;
